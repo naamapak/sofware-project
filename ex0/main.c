96e8e9ed66e2869df4b2a18b443babc593069656
root <naamap26@gmail.com>
2025-04-20 20:15:27 +0000

    actually did input

--- ex0/main.c
+++ ex0/main.c
@@ -2,31 +2,72 @@
 # include <stdlib.h>
 # include <math.h>
 
+char *_get_value(){
+    char *input = NULL;
+    char* temp = NULL;
+    char ch = getchar();
+    size_t size = 0;
+    while((ch != '\n') && ch != EOF){
+        temp = realloc(input, size + 1);
+        if (temp == NULL){
+            free(input);
+            return NULL;
+        }
 
-int _get_base(int kind) {
-/* Gets a base as input from the user. Using 's' for source base
-and 't' for target base in our program. Returns 0 for invalid base */
-    int base;
-    switch(kind){
-        case 0:
-        printf("enter source base\n");
-        break;
-        case 1:
-        scanf("enter target base\n");
+        input = temp;
+        input[size] = ch;
+        size++;
+        ch = getchar();
     }
-    scanf("%d", &base);
-    if((base < 2) || (base > 16)){
-        printf("invalid base: %d", &base);
-        return 0;
+
+    temp = realloc(input, size + 1);
+    if (temp == NULL) {
+        printf("Memory allocation failed.\n");
+        return NULL;
     }
-    else {
-        return base;
-    }
+    input = temp;
+    input[size] = '\0';
+    return input;
 }
 
-/*
+int _get_base(){
+    char *value = _get_value();
+    int result = atoi(value);
+    free(value);
+    if(result > 16 || result < 2){
+        printf("Invalid base\n");
+        return 0;
+    }
+    return result;
+}
+
+int get_source_base(){
+    printf("Enter source base:\n");
+    return _get_base();
+}
+
+int get_target_base(){
+    printf("Enter target base:\n");
+    return _get_base();
+}
+
+
 char* compute_base(int source, int target, char* value){
-    char[] digits = "0123456789ABCDEF"
-    int base10 = 0;
-} */
+    char* digits;
+    digits = "0123456789ABCDEF";
+    char* result = malloc(floor(sizeof(value) * log10(source) / log10(target)) + 1);
+    
+}
 /* How do I not use arrays? Just malloc everything? */
+
+int main(){
+    int source = get_source_base();
+    int target = get_target_base();
+    if(source * target == 0){
+        printf("Invalid input\n");
+        return 1;
+    }
+    char *value = _get_value();
+    // do actual base conversion
+    return 0;
+}
