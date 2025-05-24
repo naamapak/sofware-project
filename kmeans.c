4c7b6ad52c81675872bc604be74e448312d1b422
root <naamap26@gmail.com>
2025-05-24 19:06:39 +0000

    Added attempt at clustering

--- kmeans.c
+++ kmeans.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 double EPSILON = 0.001;
+size_t data_size = 0;  // Global variable to hold the number of data lines
 
 typedef struct datapoint datapoint;
 typedef struct cluster cluster;
@@ -22,11 +23,11 @@ typedef struct cluster {
 } cluster;
 
 
-double distance(datapoint p, cluster c){
+double distance(datapoint* p, cluster* c){
     // Computes distance from datapoint to cluster. Assumes equal lengths
     double sum = 0;
-    for(int i = 0; p.vector[i] != '\0'; i++){
-        sum += pow(p.vector[i], 2) + pow(c.vector[i], 2);
+    for(int i = 0; i < p->vector_size; i++){
+        sum += pow(p->vector[i], 2) + pow(c->vector[i], 2);
     };
     return sqrt(sum);
 }
@@ -45,7 +46,9 @@ void copy_vector(cluster* dest, const datapoint* src) { // Used for creating the
 }
 
 datapoint** _read_input(){
-    size_t data_size = 0;
+    /* Reads input from stdin using getchar.
+    not null-terminating anything, keeping track of size in data_size
+    Keeps sizes of each line */
     size_t prev_len;
     datapoint** result = malloc(sizeof(datapoint));
     double curr_d;
@@ -53,13 +56,13 @@ datapoint** _read_input(){
     while(c == EOF || c == '\0' || c == ' '){
         c = getchar();
     }
-    while(c != EOF){
+    while(c != EOF){ // Iterate over file
         size_t line_len = 0;
         double* line = malloc(sizeof(double));
-        while(c != '\n'){
+        while(c != '\n'){ // Iterate over line
             size_t len = 0;
             char* current_num = malloc(sizeof(char));
-            while(c != ',' && c != '\n'){
+            while(c != ',' && c != '\n'){ // Iterate over number
                 current_num[len] = c;
                 current_num = realloc(current_num, (++len) * sizeof(char));
                 if(!current_num){
@@ -77,10 +80,12 @@ datapoint** _read_input(){
             }
             c = getchar();
         }
-    if(data_size > 1 && line_len != prev_len){
-            printf("Invalid input %ld %ld\n", line_len,prev_len);
+
+    if(data_size > 1 && line_len != prev_len){ // Validates lines being the same length
+            printf("Invalid input\n"); // TODO make this the correct 
             exit(1);
         }
+
     prev_len = line_len;
     datapoint* p = malloc(sizeof(datapoint));
     p->vector = line;
@@ -114,7 +119,9 @@ void add_point(cluster c, datapoint p){
     if(p.centroid == &c){
         return;
     }
-    _remove_point(p.centroid, p);
+    if(p.centroid){
+        _remove_point(p.centroid, p);
+    }
     c.points = realloc(c.points, (++c.cluster_size) * sizeof(datapoint));
     if(!c.points){
         printf("An Error Has Occured\n");
@@ -143,34 +150,54 @@ void update_vector(cluster c){
 
 
 cluster* do_cluster(datapoint** data, int k, int iter){
-    int converged = 1;
     cluster* result = malloc(sizeof(cluster) * k);
+    cluster* curr_cent;
+    double curr_delta;
+    double loop_delta;
     for(int i = 0; i < k; i++){ // Initialize k clusters
         copy_vector(result[i].vector, data[i]->vector);
         result[i].points = malloc(sizeof(datapoint));
         result[i].points[0] = *data[i];
     }
     for(int i = 0; i < iter; i++){
-        // for each datapoint p:
-            // cent = p.centroid
-            // delta = distance(p, p.centroid)
-            // converged &= distance <= epsilon
-            // for each cluster c:
-                // if distance(p, c) < delta:
-                // delta = distance(p, c)
-                // cent = c;
-            // add_point(cent, p);
-        // for each cluster c:
-            // updaet_vector(c);
+        int converged = 1;
+        for(int j = 0; j < data_size; j++){
+            curr_cent = data[j]->centroid;
+            curr_delta = distance(data[j], curr_cent); // TODO don't do this if centroid is null
+            converged &= curr_delta < EPSILON;
+            for(int c_idx = 0; c_idx < k; c_idx++){
+                loop_delta = distance(data[j], &result[c_idx]);
+                if(loop_delta < curr_delta){
+                    curr_delta = loop_delta;
+                    curr_cent = &result[c_idx];
+                }
+            add_point(*curr_cent, *data[j]);
+            }
+        }
+        for(int c_idx = 0; c_idx < k; c_idx++){
+            update_vector(result[c_idx]);
+        }
     }
+    return result;
 }
 
 void free_data(datapoint** data){
-    /* Free stuff */
+    /* Freeing vectors and points. Not freeing clusters to not double-free */
+    for(int i = 0; i < (int) data_size; i++){
+        free(data[i]->vector);
+        free(data[i]);
+    }
+    free(data);
     return;
 }
 
-void free_clusters(cluster* data){
+void free_clusters(cluster* data, int k){
+    cluster c = data[0];
+    for(int i = 0; i < k; i++){
+        free(c.vector);
+        free(c.points);
+    }
+    free(data);
     return;
 }
 
@@ -179,8 +206,8 @@ void main(){
     int iter;
     scanf("%d %d", &k, &iter);
     datapoint** data = _read_input(k);
-    for(int i = 0; i < 20; i++){
-        printf("Finally: %.4f\n", data[i]->vector[0]);
 
-    }
+    cluster* result = do_cluster(data, k, iter);
+    free_data(data);
 }
+
